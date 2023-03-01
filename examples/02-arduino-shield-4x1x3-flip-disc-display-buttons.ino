8728a1adef4f87e0e34b7b503725e2babe424127
Marcin Saj <marcin@nixietester.com>
2023-03-01 19:26:36 +0100

    Update 02-arduino-shield-4x1x3-flip-disc-display-buttons.ino

--- examples/02-arduino-shield-4x1x3-flip-disc-display-buttons.ino
+++ examples/02-arduino-shield-4x1x3-flip-disc-display-buttons.ino
@@ -1,1 +1,25 @@
+/*----------------------------------------------------------------------------------*
+ * Arduino Shield for 4 x 1x3 Flip-disc Display - buttons & display control         *
+ *                                                                                  *
+ * Project website: https://bit.ly/ARD-4x1x3                                        *
+ * Marcin Saj 28 Feb 2023                                                           *
+ * https://www.Flipo.io                                                             *
+ *                                                                                  *
+ * Arduino Nano Every is required                                                   *
+ * 1x3 Flip-disc display is required: https://bit.ly/1x3-FD                         *
+ *----------------------------------------------------------------------------------*/
+
+#include <FlipDisc.h>   // https://github.com/marcinsaj/FlipDisc
+
+// Pin declaration for Arduino Nano Every and PSPS module
+#define EN_PIN  10      // Start & End SPI transfer data
+#define CH_PIN  A0      // Charging PSPS module - turn ON/OFF
+#define PL_PIN  A1      // Release the current pulse - turn ON/OFF
+
+
+
+
+
+
+
 /* To do */
